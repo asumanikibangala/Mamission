a9d2883d68fe3a869c1fc3e02d5e6e3f3cf07aa1
asumanikibangala <asumanikibangala918@gmail.com>
2026-02-26 08:45:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Menu hamburger pour mobile
+        const hamburger = document.querySelector(".hamburger");
+        const navMenu = document.querySelector(".nav-menu");
+
+        hamburger.addEventListener("click", () => {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+        });
